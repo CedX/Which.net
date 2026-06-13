96d13f213d3888424113affe0b8a0ee2eef4e15a
Cédric Belin <cedx@outlook.com>
2026-06-13 17:19:10 +0200

    Fix a regression [skip ci]

--- Scripts/Cmdlets.psm1
+++ Scripts/Cmdlets.psm1
@@ -43,7 +43,7 @@ function Invoke-DotNetTest {
 	Creates a new Git tag.
 #>
 function New-GitTag {
-	[SuppressMessage("PSUseShouldProcessForStateChangingFunctions")]
+	[SuppressMessage("PSUseShouldProcessForStateChangingFunctions", "")]
 	param (
 		# The tag name.
 		[Parameter(Mandatory, Position = 0)]
